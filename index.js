7055fb575ac777272a132908fc3aec90f1553a4e
Mehedad1948 <mnourib12@gmail.com>
2024-08-25 19:11:35 +0330

    Add page opener loop

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 
-async function main() {
-    const browser = await puppeteer.launch({ executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', headless: false });
+const cheerio = require('cheerio');
+const fs = require('fs');
 
-    const page = await browser.newPage()
-    await page.goto('https://pptr.dev/guides/configuration')
+async function listing(page) {
+ 
+
+    await page.goto('https://greatfalls.craigslist.org/search/trp#search=1~list~0~0', { waitUntil: 'networkidle2' });
+
+    const html = await page.content();
+
+    const $ = cheerio.load(html);
+
+    fs.writeFileSync('./test.html', html);
+    const titles = $('.cl-static-search-result');
+    const data = []
+    let list = titles.each((index, element) => {
+        let title = $($(element).find('.title ')[0]).text().trim()
+        let link = $($(element).find('a')[0]).attr('href').trim()
+        let price = $($(element).find('.price')[0]).text().trim().replace('$', '')
+        let location = $($(element).find('.location')[0]).text().trim()
+        data.push({ title, link, price, location })
+    });
+
+    return data
 }
 
+async function jobDescriptions(list, page) {
+    
+    for(var i= 0; i< list.length; i++) {
+        console.log('🦩🦩',list[i].link);
+        
+        await page.goto(list[i].link)
 
-main()
+        const html = await page.content()
+    }
+}
+
+async function main() {
+    const browser = await puppeteer.launch({
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        headless: false,
+    });
+
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36');
+    const list = await listing(page)
+    const listWithJobDescription = await jobDescriptions(list, page)
+    // await browser.close();
+}
+
+main();
