eb381c54e1ede7273eec0b2a6cef11eaecd09824
Mehedad1948 <mnourib12@gmail.com>
2024-08-25 15:40:10 +0330

    Table Selector

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 
 async function main(params) {
-    const html = await request.get('https://www.carpetdepo.eu/iranian-carpets')
+    const html = await request.get('https://www.javatpoint.com/html-table')
     fs.writeFileSync('./test.html', html)
     const $ = await cheerio.load(html)
-   const title =  $('title').text()
-   $('h2').each((index, element)=> {
-    console.log($(element).text());
-    
-   })
-   
+    const title = $('title').text()
+
+    $('table tr').each((index, element) => {
+        const tds = $(element).find('td')
+        const content = $(tds[0]).text()
+        console.log('🔥', content);
+    });
+
+
 }
 
 main()
